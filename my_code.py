66fc4465059ab728b57aaf4ebd376c82b33db6bf
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-08-31 18:49:48 -0700

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -1,7 +1,7 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
-def inc(x):
+def cast(x):
     # User code goes here
     return x + 1
 
@@ -9,7 +9,7 @@ def inc(x):
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    print(inc(6))
+    print(cast(6))
 
     # After you are satisfied with your results, use input() to prompt the user for a value:
     #v = input("Value to increment: ")
