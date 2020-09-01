0886860a7e3e8889d185e67863e8593032430a32
Megan Leich <mleich@athenian.org>
2020-09-01 02:01:07 +0000

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -1,7 +1,7 @@
-from my_code import inc
+from my_code import times_3
 
 
-def test_inc():
-    assert 5 == inc(4)
-    assert 1 == inc(0)
-    assert 0 == inc(-1)
+def test_times_3():
+    assert 15 == times_3("5")
+    assert 0 == times_3("0")
+    assert -3 == times_3("-1")
